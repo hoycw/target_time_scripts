3a5a6678f6d4749e33adc8f23a357528423fcbe5
ImGriffithCogsciBerk <imgriffith@berkeley.edu>
2017-11-20 12:08:11 -0800

    Log_Changes
    List of alterations to Target_Time code

---
+++ target_time_change_log.py
@@ -0,0 +1,37 @@
+#===================================
+#  Log of alterations to experiment
+#===================================
+
+
+
+
+# 1.9.2.test: modularized parameters, log, and variables to cut down on file length 
+# 1.9.2: different trigger codes for stim (1) and feedback (2); stim trigger lasts until feedback_delay; adv_scr_txt moved down to avoid overlap
+# 1.9.1: EEG triggers actually work after troubleshooting on 135D (must use WinXP)
+# 1.9: EEG triggers; no photodiode, screen size 
+# 1.8.5: ITIs=[0.2,0.4,0.7,1.0]; feedback_dur=0.8; log:(paradigm_version,n_fullvis,n_training);
+#   Adi-prevent 3 blocks of same type in a row; quit on title screen
+# 1.8.4: response marker is wider; block left during break; fixed resp_marker_width bug
+# 1.8.3: ITIs=[0.3,0.65,1]; four blocks per condition; break min = 30s; advance option only appears after break_min_dur;
+#       added total blocks to block start screen (e.g., 1/8); changed speed/distance instructions to be simpler (it's always the same time)
+# 1.8.2: 15 training trials, tolerance_init = {'easy': 0.125, 'hard': 0.05}, block order = e e h h; fix block_order bug
+# 1.8.1: Optimized for ECoG Laptops
+# 1.8 fixes:
+#   -more clear instructions
+#   -screen delay following block start for preparation
+#   -fixed quit checks (stopped calling responses "key")
+#   -bullseye color switch, block score +1 correction
+# 1.7 many fixes:
+#   -cuts down the MASSIVE log file (hopefully avoids crashes)
+#   -tolerance limits, adjustments during training, adjust after logging, center bullseye = tolerance min
+#   -display point total after blocks (previous block and total)
+#   -more spaced out ITIs to reduce rhythm
+#   -wider response marker, breaks after all blocks (maybe v1.6 too?), mention time out on break instructions
+# 1.6 implements logging and saving the data
+# 1.5 implements block structure to whole experiment, instructions, training (covered=False examples)
+# 1.4 got skipped (handler nonsense)
+# 1.3 added photodiode trigger rectangle, basic logging
+#   (incomplete logging, to be finished once trail/exp ahndelres are done)
+# 1.2 has more comments than 1.1
+# implements staircase adjustments to target intervals
+# dict for all variables changing across trial types (easy/hard)
